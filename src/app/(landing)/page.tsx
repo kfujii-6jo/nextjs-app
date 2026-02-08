d5e9240bc6b34610f59f913b176d87bbb9a7fd5e
kfujii-6jo <kfujii.dev@gmail.com>
2026-02-08 15:50:23 +0900

    feat: lpの全体を作成

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,9 +1,17 @@
+import { CallToAction } from "@/components/call-to-action";
+import { Features } from "@/components/features";
+import { Footer } from "@/components/footer";
+import { Hero } from "@/components/hero";
 import { LandingHeader } from "@/components/landing-header";
 
 const LandingPage = async () => {
   return (
     <main className="min-h-screen">
       <LandingHeader />
+      <Hero />
+      <Features />
+      <CallToAction />
+      <Footer />
     </main>
   );
 };
